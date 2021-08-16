5dd316565005918323dbb78a9ad7289c54e924cd
Jordan Fisher <thementalgoose@gmail.com>
2021-08-16 19:52:26 +0100

    Updated project dependencies, more examples, and README

--- functions/src/modules/sample.functions.ts
+++ functions/src/modules/sample.functions.ts
@@ -2,21 +2,55 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import firestore from '../lib/admin';
 import { DocumentSnapshot, Timestamp } from '@google-cloud/firestore';
+import axios, { AxiosResponse } from 'axios';
 
 /**
- * Setup a firebase firestore function
+ * Example Firebase Firestore function
  */
 export const sampleFunction = functions.firestore
     .document("Collection/{documentId}")
-    .onCreate((snap: DocumentSnapshot, context: functions.EventContext) => { 
+    .onCreate(async (snap: DocumentSnapshot, context: functions.EventContext) => { 
         // React to a document being added to a collection
+
+        // EXAMPLE: Send a GET request 
+        const getRequest: AxiosResponse<any> = await axios.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        console.log(getRequest.data);
+
+        return;
     });
 
 /**
- * Setup an firebase auth function
+ * Example Firebase Auth function
  */
 export const authCreate = functions.auth
     .user()
-    .onCreate((user: admin.auth.UserRecord, context: functions.EventContext) => { 
+    .onCreate(async (user: admin.auth.UserRecord, context: functions.EventContext) => { 
         // React to a user being created
+
+        // EXAMPLE: Save something to firestore
+        await firestore
+            .collection("SampleCollection")
+            .add({"user-created": true});
+
+        return;
     })
+
+/**
+ * Example HTTPS request
+ */
+export const postRequest = functions
+    .runWith({
+        timeoutSeconds: 60,
+        memory: '256MB'
+    })
+    .https
+    .onRequest(async (req, res) => { 
+        // React to a POST or GET request to https://<region>-<project-id>.cloudfunctions.net/function-name.
+        // Look in the functions tab in the console to find out more
+
+        // EXAMPLE: Send a POST request
+        const postRequest: AxiosResponse<any> = await axios.post(`https://sample.com/login`, { 'username': 'username', 'password': 'password' });
+        res.status(200).send(postRequest.data);
+
+        return;
+    });
